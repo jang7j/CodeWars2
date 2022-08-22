5e2adcc8ffd2cfd1ad9deb0fadaf13c52460b50c
Yun Jang <jang7j@yahoo.com>
2022-08-22 09:24:05 -0500

    initial commit

---
+++ CodeWars2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+
+//Calculate the average of numbers in given array
+//Empty arrays should return 0
+
+var array = new double[] { 1.0, 2.0, 3.0 };
+
+static double FindAverage(double[] array)
+{
+    return array.Length == 0 ? 0 : array.Average();
+}
+
+Console.WriteLine(FindAverage(array)); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
